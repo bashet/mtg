c3caa5f5a0c662e935acca701829c83e7f34f05a
bashet <a_bashet@yahoo.com>
2018-06-13 03:20:48 +0100

    try to add module for user, role and config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,7 @@
     </div>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @stack('scripts')
     @include('sweet::alert')
     <script>
         $('#flash-overlay-modal').modal();
