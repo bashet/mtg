cb530c55d106b2d7fdf6fc4a42e9006883a1720e
bashet <a_bashet@yahoo.com>
2018-06-14 19:06:37 +0100

    loading one image only

--- Modules/Mtg/Resources/views/carousel.blade.php
+++ Modules/Mtg/Resources/views/carousel.blade.php
@@ -1,11 +1,11 @@
 <div class="carousel-inner">
-    @if($i = 1)
+    @if($cards->count() && $i = 1)
         @foreach($cards as $card)
-            <div class="carousel-item {{$i++ == 1 ? 'active' : ''}}">
+            <div class="carousel-item {{$i == 1 ? 'active' : ''}}">
                 <div class="row">
                     <div class="col-sm-6">
                         <h3 class="card-title text-white">{{$card->cardName}}</h3>
-                        <img class="d-block w-100" src="{{url($card->front)}}" alt="{{$card->cardName}}">
+                        <img class="d-block w-100" {{$i > 1 ? 'data-': ''}}src="{{url($card->front)}}" alt="{{$card->cardName}}">
                     </div>
                     <div class="col-sm-6 pt-5">
                         <p class="card-text text-white">{{$card->cardType}}</p>
@@ -16,6 +16,7 @@
                     </div>
                 </div>
             </div>
+            @php($i++)
         @endforeach
     @endif
 </div>
