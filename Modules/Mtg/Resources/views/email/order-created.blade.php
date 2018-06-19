190d5f548b2b25266a3698843631d8c58b83ebb2
bashet <a_bashet@yahoo.com>
2018-06-19 16:26:05 +0100

    table is showing nicely in mail

--- Modules/Mtg/Resources/views/email/order-created.blade.php
+++ Modules/Mtg/Resources/views/email/order-created.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
-    {{-- Body --}}
+    Dear {{$order->name}},<br><br>
+    Thank you for your order. Please find below what have ordered.<br>
+    <br>
     @component('mail::table')
-        | Item       | Unit Price         | Quantity  |  Total
-        | ------------- |:-------------:| --------:| ------:|
-        @foreach($order->items as $item)
-            | {{$item->card->cardName}}     | {{number_format($item->card->cardPrice, 2)}}      | {{$item->quantity}}      |    {{number_format($item->card->cardPrice * $item->quantity, 2)}} |
-        @endforeach
-    @endcomponent
-
-
-    Thanks,
+    | Item | Unit Price | Quantity | Total |
+    | ---- | : -------: | --------: | ----: |
+    @foreach($order->items as $item)
+        |{{$item->card->cardName}}|{{number_format($item->card->cardPrice, 2)}}|{{$item->quantity}}|{{number_format($item->card->cardPrice * $item->quantity, 2)}}|
+    @endforeach
+    @endcomponent<br>
+    <br>Thanks,<br>
     {{ config('app.name') }}
 @endcomponent
 
